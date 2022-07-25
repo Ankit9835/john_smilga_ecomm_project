e1124a012f3d600649095c143fff7ab9c705a05d
ankit9835 <ankydchrismatic@gmail.com>
2022-07-25 22:21:58 +0530

    add to cart module work going on

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -23,6 +23,7 @@ const products_reducer = (state, action) => {
     const featured_products = action.payload.filter(
       (product) => product.featured === true
     )
+    console.log(featured_products)
     return {
       ...state,
       products_loading: false,
@@ -32,6 +33,23 @@ const products_reducer = (state, action) => {
   }
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_error: true }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return { ...state, single_product_loading: true }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: false,
+      single_product: action.payload,
+    }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return { ...state, single_product_error: true }
   }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
